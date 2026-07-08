c26cfa9af83eb966405ace6a5049e19e7ea40d7a
fcohailaf <fcohailaf@unjbg.edu.pe>
2026-07-08 09:57:09 -0500

    Ejercicio para hallar la suma de los elementos de una matriz cuadrada

---
+++ MatrizCuad.cpp
@@ -0,0 +1,31 @@
+//MatrizCuad.cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Ingrese la dimensión de la matriz: ";
+    cin >> n;
+
+    int matriz[100][100];
+    int suma = 0;
+
+    cout << "Ingrese los elementos de la matriz:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+        	cout << "matriz[" << i << "][" << j << "] = ";
+            cin >> matriz[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            suma = suma + matriz[i][j];
+        }
+    }
+
+    cout << "La suma de los elementos es: " << suma << endl;
+
+    return 0;
+}
